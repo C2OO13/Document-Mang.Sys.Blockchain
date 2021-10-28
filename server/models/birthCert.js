9eb0ec133482dfc2441733b6cf638c35bdb1b858
djsavaliya <savaliyadarshil12884@gmail.com>
2021-10-28 16:00:51 +0530

    DB Models Added.

---
+++ server/models/birthCert.js
@@ -0,0 +1,23 @@
+import mongoose from 'mongoose';
+
+const birthSchema = mongoose.Schema({
+    name: String,
+    guardian: String,
+    hospital: String,
+    dob: Date,
+    tob: Date,
+    location: String,
+    /*Save time as type: String
+    const userInput = '05:20';
+    const hours = userInput.slice(0, 2);
+    const minutes = userInput.slice(3);
+    Save date time in type: Date
+    But in second option you have to create Date object and set hours and minutes:
+    
+    const date = new Date(dateString);
+    date.setHours(hours, minutes); */
+});
+
+const BirthCert = mongoose.model('BirthCert', birthSchema);
+
+export default BithCert;
