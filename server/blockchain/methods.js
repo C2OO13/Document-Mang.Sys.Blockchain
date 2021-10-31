787e93fdb1dd65f58026b72582e6f112861ba8d9
C2OO13 <smitpat2000@gmail.com>
2021-10-31 08:36:39 +0530

    Blockchain methods added

--- server/blockchain/methods.js
+++ server/blockchain/methods.js
@@ -1,38 +1,62 @@
 import MainContract from "./MainContract";
+import { config } from 'dotenv';
+config({ path: '../.env' });
+import Web3 from 'web3';
+const { providers } = Web3;
+
+const web3 = new Web3(new providers.HttpProvider(process.env.INFURA_KOVAN_URL));
+const signer = web3.eth.accounts.privateKeyToAccount(process.env.SIGNER_PRIVATE_KEY);
+web3.eth.accounts.wallet.add(signer);
 
 export const getCertificate = async (id) => {
     try {
+
+        const data = await MainContract.methods
+            .getCertificate(id)
+            .call();
+
+        console.log(data);
+        return data;
+
+    } catch (error) {
+        console.log(error);
+        return error.message;
+    }
+}
+
+export const setCertificate = async (id, acc, hash, desc) => {
+    try {
         await MainContract.methods
-            .approveAgreement(agreement.investor, agreement.company)
-            .send({ from: user?.address });
-
+            .setCertificate(id, acc, hash, desc)
+            .send({ from: signer.address });
+        return "Success";
     } catch (error) {
         console.log(error);
         return error.message;
     }
 }
 
-export const setCertificate = async (id) => {
+export const setActive = async (id, state) => {
     try {
-
+        await MainContract.methods
+            .setActive(id, state)
+            .send({ from: signer.address });
+        return 'Success';
     } catch (error) {
         console.log(error);
         return error.message;
     }
 }
 
-export const setActive = async (id) => {
+export const newCertificate = async (acc, hash, desc) => {
     try {
-
-    } catch (error) {
-        console.log(error);
-        return error.message;
-    }
-}
-
-export const newCertificate = async (id) => {
-    try {
-
+        await MainContract.methods
+            .newCertificate(acc, hash, desc)
+            .send({ from: signer.address });
+        const id = await MainContract.methods
+            .lastID()
+            .call();
+        return id;
     } catch (error) {
         console.log(error);
         return error.message;
