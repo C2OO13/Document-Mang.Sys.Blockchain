bc36422f648498fd6338141ec269b848e3e738a5
Dhruv-Rana <ranadhruv975@gmail.com>
2021-12-07 10:36:58 +0530

    File Sent to Server

--- client/src/components/VerifyCertificate.js
+++ client/src/components/VerifyCertificate.js
@@ -1,24 +1,33 @@
 import React from "react"
-// import { PDFDocument } from "pdf-lib"
 import axios from "../api"
-import getDataFromPDF from '../helper/readPDF.js';
 
 const VerifyCertificate = () => {
   const handleSubmit = async (event) => {
     event.preventDefault()
-    console.log(event.target[1].files[0]);
-    const file = event.target[1].files[0]
+    const file = event.target[0].files[0]
     if (file.type !== "application/pdf") {
       alert("Please upload file in pdf format only!")
       return
     }
-    
-    const data = getDataFromPDF(file.name);
-    console.log("123121321")
-    console.log(data)
+
+    let formData = new FormData()
+    formData.append("file", file)
+    formData.append("name", file.name)
+    const { data } = await axios.post("/api/verify_pdf", formData, {
+      headers: {
+        "Content-Type": "multipart/form-data",
+      },
+    })
+
+    // data.data has info about certificate being uploaded
+
+    if (data.verification) {
+      alert("Verified!")
+    } else {
+      alert("Not Verified!")
+    }
+
     // await axios.post("/api/get_certificate", keywords[0]);
-
-
 
     // const pdfBytes = fs.readFile('./abc.pdf').then((res) => res.arrayBuffer())
     // console.log(pdfBytes)
@@ -26,9 +35,6 @@ const VerifyCertificate = () => {
     // const pdfDoc = await PDFDocument.load(pdfBytes, {
     //   updateMetadata: false,
     // })
-    // console.log("asdasdasdasdadasdasd");
-
-
 
     // // Read all available metadata fields
     // const title = pdfDoc.getTitle()
@@ -50,10 +56,6 @@ const VerifyCertificate = () => {
         style={{ width: "1000px", margin: "0 auto" }}
         onSubmit={handleSubmit}
       >
-        <label>Enter a shareable link to the certificate:</label>
-        <input type="url" required />
-        <br />
-        <br />
         <label>Upload Certificate:</label>
         <input type="file" accept=".pdf" required />
         <br />
