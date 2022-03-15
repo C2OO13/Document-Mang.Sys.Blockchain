c3175fd8da3f08dd6f65f9f1ab6ac0b74438fc8a
Darshil Savaliya <savaliyadarshil12884@gmail.com>
2022-03-15 21:01:36 +0530

    IPFS Test

---
+++ server/try.js
@@ -0,0 +1,11 @@
+import fs from 'fs'
+import path from 'path'
+import {create} from 'ipfs-http-client'
+
+const ipfs = create()
+
+const data = fs.readFileSync(path.resolve(process.cwd(),'../pdf_file/AlteredData.pdf'))
+// console.log(data)
+
+const { cid } = await ipfs.add(data)
+console.log(cid)
