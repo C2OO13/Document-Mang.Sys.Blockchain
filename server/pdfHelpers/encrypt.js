d01b8b38a4083bdd615facaa52c43caa25dbd11c
C2OO13 <smitpat2000@gmail.com>
2022-05-11 12:23:37 +0530

    Encryption function added

---
+++ server/pdfHelpers/encrypt.js
@@ -0,0 +1,15 @@
+import HummusRecipe from 'hummus-recipe'
+
+// const HummusRecipe = require('hummus-recipe');
+
+export const encyptPdf = async (input, output, password) => {
+    const pdfDoc = new HummusRecipe(input, output);
+
+    pdfDoc
+        .encrypt({
+            userPassword: password,
+            ownerPassword: password,
+            userProtectionFlag: 4
+        })
+        .endPDF();
+}
